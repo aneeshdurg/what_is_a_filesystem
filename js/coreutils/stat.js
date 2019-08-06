1783f98e779600db8c4379052fb8b77635d4edb7
Aneesh Durg <durg2@illinois.edu>
2019-08-06 09:27:21 -0400

    Fixed bug with stat

--- js/coreutils/stat.js
+++ js/coreutils/stat.js
@@ -11,11 +11,11 @@ Shell.prototype.handle_stat = async function(command) {
     stat_str += "Size: " + info.filesize + "B\n";
     stat_str += "Access: 0o" + info.mode.toString(8) + "\n";
 
-    atim = new Date(info.atim);
+    var atim = new Date(info.atim);
     stat_str += "Access: " + atim.toDateString() + " " + atim.toTimeString() + "\n";
-    mtim = new Date(info.mtim);
+    var mtim = new Date(info.mtim);
     stat_str += "Modify: " + mtim.toDateString() + " " + mtim.toTimeString() + "\n";
-    ctim = new Date(info.ctim);
+    var ctim = new Date(info.ctim);
     stat_str += "Change: " + ctim.toDateString() + " " + ctim.toTimeString() + "\n";
 
     var error = this.filesystem.write(command.output, str_to_bytes(stat_str));
